d2d70f660b4e25877f572a036d302feb2a431670
mpetrovych <markian.petrovych@gmail.com>
2025-01-16 02:12:08 +0200

    Added Player
    Player class with turn and color (needs to check and change them)

---
+++ Chess/ChessLogic/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public enum Player
+    {//колір фігури
+        None,
+        White,
+        Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {// зміна ходу через switch/case
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+        }
+    }
+}
