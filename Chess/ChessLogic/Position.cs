c5e06f26391f924491cd2a6c40f738eddeaa5a29
mpetrovych <markian.petrovych@gmail.com>
2025-01-16 02:18:55 +0200

    Update Position.cs

--- Chess/ChessLogic/Position.cs
+++ Chess/ChessLogic/Position.cs
@@ -8,5 +8,48 @@ namespace ChessLogic
 {
     internal class Position
     {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        { //констр
+            Row = row;
+            Column = column;
+        }
+
+        public Player SquareColor()
+        { //колір коітки, парні - білий
+            if ((Row + Column) % 2 == 0)
+            {
+                return Player.White;
+            }
+
+            return Player.Black;
+        }
+
+        //Овверайд для використання Порівняння
+        public override bool Equals(object obj)
+        { 
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Column);
+        }
+
+        public static bool operator ==(Position left, Position right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
+
+
     }
 }
